d19cf593f010f3089283b3413a174fc5146016c1
Alex <al.cayuqueo@duocuc.cl>
2024-07-01 03:35:41 -0400

    ultimos ajustes

--- app_taller/urls.py
+++ app_taller/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'app_taller'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('contacto', views.contacto, name='contacto'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('productos/', views.productos, name='productos'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('quienessomos/', views.quienessomos, name='quienessomos'),
 ]
--- app_taller/views.py
+++ app_taller/views.py
@@ -7,6 +7,22 @@ def index(request):
     context={}
     return render(request, 'app_taller/index.html', context)
 
+def register(request):
+    context={}
+    return render(request, 'app_taller/register.html', context)
+
+def login(request):
+    context={}
+    return render(request, 'app_taller/login.html', context)
+
+def productos(request):
+    context={}
+    return render(request, 'app_taller/productos.html', context)
+
 def contacto(request):
     context={}
     return render(request, 'app_taller/contacto.html', context)
+
+def quienessomos(request):
+    context={}
+    return render(request, 'app_taller/quienessomos.html', context)
